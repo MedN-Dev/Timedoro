921d5d8890e1c419148771668e1956b9a75edfbb
Gustavo Caetano <souljacker@gmail.com>
2017-11-12 01:51:36 -0200

    Pomodoros working

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-flex xs12>
-    <v-card-text class="counter">{{ pomodoro }}</v-card-text>
+    <v-card-text class="counter">{{ time }}</v-card-text>
     <v-layout row wrap class="buttons">
       <v-flex xs6>
         <v-card dark raised color="green darken-3" class="button">
@@ -11,7 +11,8 @@
         <v-card dark color="primary" class="button">
           <div @click="reset">Reset</div> 
         </v-card>
-      </v-flex>    
+      </v-flex>
+      {{state}}    
     </v-layout>    
   </v-flex>  
 </template>
@@ -28,10 +29,10 @@ export default {
     };
   },
   methods: {
-    ...mapMutations(['startTime', 'reset']),
+    ...mapMutations(['startTime', 'reset', 'setStage', 'setPomodoros', 'nextTimer']),
   },
   computed: {
-    ...mapGetters(['timer', 'pomodoro']),
+    ...mapGetters(['timer', 'time', 'state', 'pomodoros', 'stage']),
     startButton() {
       if (this.timer.playing === false) {
         return 'Play';
@@ -39,7 +40,28 @@ export default {
       return 'Pause';
     },
   },
+  watch: {
+    time(current) {
+      if (current === '00:00') {
+        if (this.stage === 'pomodoro') {
+          // if second pomodoro in a row - long break
+          if (this.pomodoros % 2 === 0) {
+            this.setStage('long');
+          } else {
+            // if first pomodoro in a row - short break
+            this.setStage('short');
+          }
+        } else {
+          // coming out of a short
+          this.setStage('pomodoro');
+          this.setPomodoros(this.pomodoros + 1);
+        }
+        this.nextTimer();
+      }
+    },
+  },
   mounted() {
+
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -7,31 +7,59 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
+    stage: null,
+    pomodoros: 0,
     timer: {
       playing: false,
-      pomodoro: 25 * 60,
-      short: 5 * 60,
-      long: 10 * 60,
-      time: 25 * 60,
+      pomodoro: 0.05 * 60,
+      short: 0.02 * 60,
+      long: 0.03 * 60,
+      time: null,
       obj: null,
     },
   },
   getters: {
+    state(state) {
+      return state;
+    },
+    stage(state) {
+      return state.stage;
+    },
     timer(state) {
       return state.timer;
     },
-    pomodoro(state) {
-      let minutes = parseInt(state.timer.time / 60, 10);
-      let seconds = parseInt(state.timer.time % 60, 10);
+    pomodoros(state) {
+      return state.pomodoros;
+    },
+    time(state) {
+      if (state.timer.time === null) {
+        state.timer.time = state.timer.pomodoro;
+      }
 
-      minutes = minutes < 10 ? '00' : minutes;
-      seconds = seconds < 10 ? '00' : seconds;
+      const time = state.timer.time;
+      // format time for output
+      let minutes = parseInt(time / 60, 10);
+      let seconds = parseInt(time % 60, 10);
+
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      seconds = seconds < 10 ? `0${seconds}` : seconds;
 
       return `${minutes}:${seconds}`;
     },
   },
   mutations: {
+    setStage(state, value) {
+      state.stage = value;
+    },
+    setPomodoros(state, value) {
+      state.pomodoros = value;
+    },
     startTime(state) {
+      // if new timer, set stage to pomodoro
+      if (state.stage === null) {
+        state.stage = 'pomodoro';
+        state.timer.time = state.timer.pomodoro;
+      }
       if (state.timer.playing === false) {
         state.timer.playing = true;
         state.timer.obj = setInterval(() => {
@@ -46,9 +74,20 @@ const store = new Vuex.Store({
         clearInterval(state.timer.obj);
       }
     },
+    nextTimer(state) {
+      if (state.stage === 'pomodoro') {
+        state.timer.time = state.timer.pomodoro;
+      } else if (state.stage === 'short') {
+        state.timer.time = state.timer.short;
+      } else {
+        state.timer.time = state.timer.long;
+      }
+    },
     reset(state) {
       state.timer.playing = false;
       state.timer.time = state.timer.pomodoro;
+      state.pomodoros = 0;
+      state.stage = null;
       clearInterval(state.timer.obj);
     },
   },
